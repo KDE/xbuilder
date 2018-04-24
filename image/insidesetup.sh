91aeb4bf21ab3bb08e65aa6ef8ae80d7ef936cd2
Bhushan Shah <bhush94@gmail.com>
2018-04-24 22:21:31 +0530

    Changes for next version
    - use neon repo
    - add gpg key properly

--- image/insidesetup.sh
+++ image/insidesetup.sh
@@ -6,6 +6,7 @@ unset LANG #remove? or just install locales...
 source /etc/environment
 
 apt update
+
 apt-get install wget -y
 
 dpkg --add-architecture armhf
@@ -22,19 +23,14 @@ deb-src [arch=i386,amd64] http://archive.ubuntu.com/ubuntu xenial main universe 
 deb [arch=i386,amd64] http://archive.ubuntu.com/ubuntu xenial-updates main universe restricted
 deb-src [arch=i386,amd64] http://archive.ubuntu.com/ubuntu xenial-updates main universe restricted
 
-deb [arch=armhf,amd64] http://mobile.neon.pangea.pub xenial main
-deb-src [arch=armhf,amd64] http://mobile.neon.pangea.pub xenial main
+deb [arch=armhf,amd64] http://archive.neon.kde.org/dev/unstable xenial main
+deb-src [arch=armhf,amd64] http://archive.neon.kde.org/dev/unstable xenial main
 " > /etc/apt/sources.list
 
 echo 'Debug::pkgProblemResolver "true";' > /etc/apt/apt.conf.d/debug
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A12B6139432062D1
-
-pushd /tmp
-rm key || true
-wget http://mobile.kci.pangea.pub/Pangea%20CI.gpg.key -O key
-apt-key add key
-popd
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 444DABCF3667D0283F894EDDE6D4736255751E5D 
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E47F5011FA60FC1DEBB1998933056FA14AD3A421
 
 apt update
 apt-get install crossbuild-essential-armhf adduser dh-exec vim zsh git htop python sudo cmake-curses-gui -y
@@ -59,7 +55,10 @@ CC=/usr/bin/arm-linux-gnueabihf-gcc
 VARS=\`dpkg-architecture -A amd64 -a armhf -l\`
 export \$VARS
 export PATH=/home/plasmamobile/xutils:\$PATH
-export QMAKESPEC=ubuntu-arm-gnueabihf-g++
 EOF
 
-apt-get build-dep -a armhf kwin -y --force-yes #--host-architecture=armhf # force-yes is needed because "WARNING: The packages cannot be authenticated!"
+/home/plasmamobile/xutils/createfakepkg gamin && dpkg -i gamin.deb && rm /gamin.deb
+/home/plasmamobile/xutils/createfakepkg libgamin0 && dpkg -i libgamin0.deb && rm /libgamin0.deb
+
+apt-get install libkf5config-bin-dev:amd64 libkf5config-bin-dev:armhf libkf5coreaddons-dev:amd64 libkf5coreaddons-dev-bin:amd64 libkf5coreaddons-dev:armhf kirigami2-dev:armhf phonon4qt5-backend-gstreamer:armhf qtdeclarative5-dev:armhf -y
+apt-get -a armhf build-dep kirigami -y
