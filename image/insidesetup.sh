8610cdcef05355ffca61e8db9081dacf79a46774
Bhushan Shah <bhush94@gmail.com>
2018-04-24 23:24:18 +0530

    optimize the dockerfile

--- image/insidesetup.sh
+++ image/insidesetup.sh
@@ -5,38 +5,6 @@ unset LANG #remove? or just install locales...
 
 source /etc/environment
 
-apt update
-
-apt-get install wget -y
-
-dpkg --add-architecture armhf
-
-echo "deb [arch=armhf] http://ports.ubuntu.com/ xenial main universe restricted
-deb-src [arch=armhf]  http://ports.ubuntu.com/ xenial main universe restricted
-
-deb [arch=armhf] http://ports.ubuntu.com/ xenial-updates main universe restricted
-deb-src [arch=armhf] http://ports.ubuntu.com/ xenial-updates main universe restricted
-
-deb [arch=i386,amd64] http://archive.ubuntu.com/ubuntu xenial main universe restricted
-deb-src [arch=i386,amd64] http://archive.ubuntu.com/ubuntu xenial main universe restricted
-
-deb [arch=i386,amd64] http://archive.ubuntu.com/ubuntu xenial-updates main universe restricted
-deb-src [arch=i386,amd64] http://archive.ubuntu.com/ubuntu xenial-updates main universe restricted
-
-deb [arch=armhf,amd64] http://archive.neon.kde.org/dev/unstable xenial main
-deb-src [arch=armhf,amd64] http://archive.neon.kde.org/dev/unstable xenial main
-" > /etc/apt/sources.list
-
-echo 'Debug::pkgProblemResolver "true";' > /etc/apt/apt.conf.d/debug
-
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 444DABCF3667D0283F894EDDE6D4736255751E5D 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E47F5011FA60FC1DEBB1998933056FA14AD3A421
-
-apt update
-apt-get install crossbuild-essential-armhf adduser dh-exec vim zsh git htop python sudo cmake-curses-gui -y
-
-
-adduser plasmamobile --gecos "" --disabled-password
 su - plasmamobile -c "git clone git://anongit.kde.org/xutils.git /home/plasmamobile/xutils"
 echo "plasmamobile   ALL=NOPASSWD:ALL" >> /etc/sudoers
 
