e460e6a0f5312d88c5f53df12e024f9397f632af
Bhushan Shah <bhush94@gmail.com>
2018-04-26 16:02:19 +0530

    Create dummy libgamin package
    And copy the libgamin0 library to arch specific path to not break
    kcoreaddons

--- image/insidesetup.sh
+++ image/insidesetup.sh
@@ -14,5 +14,28 @@ export \$VARS
 export PATH=/home/plasmamobile/xutils:\$PATH
 EOF
 
-apt-get install libkf5config-bin-dev:amd64 libkf5config-bin-dev:armhf libkf5coreaddons-dev:amd64 libkf5coreaddons-dev-bin:amd64 libkf5coreaddons-dev:armhf kirigami2-dev:armhf phonon4qt5-backend-gstreamer:armhf qtdeclarative5-dev:armhf -y
+RUN createfakepkg gamin && dpkg -i gamin.deb && rm gamin.deb
+RUN createfakepkg libgamin0 && dpkg -i libgamin0.deb && rm libgamin0.deb
+
+cd /tmp
+
+apt-get download libgamin0:amd64
+dpkg -x libgamin0*amd64.deb libgamin-amd64
+mv libgamin-amd64/usr/lib/* /usr/lib/x86_64-linux-gnu/
+rm -r libgamin*
+
+apt-get download libgamin0:armhf
+dpkg -x libgamin0*armhf.deb libgamin-armhf
+mv libgamin-armhf/usr/lib/* /usr/lib/arm-linux-gnueabihf/
+rm -r libgamin*
+
+apt-get install libkf5config-bin-dev:amd64 \
+    libkf5config-bin-dev:armhf \
+    libkf5coreaddons-dev:amd64 \
+    libkf5coreaddons-dev-bin:amd64 \
+    libkf5coreaddons-dev:armhf \
+    kirigami2-dev:armhf \
+    phonon4qt5-backend-gstreamer:armhf \
+    qtdeclarative5-dev:armhf -y
+
 apt-get -a armhf build-dep kirigami -y
